44c5713bd58568224d3af3920fb90015986b0f4f
Sashank Pindiproli <sashankpindiproli@Sashanks-MacBook-Pro.local>
2017-11-18 21:12:56 -0800

    express framework implementation

---
+++ ExpressTutorial/server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('images'));
+
+app.get('/', function(request, response) {
+  console.log("Got a GET request for the homepage");
+  response.send('Hello World');
+});
+
+app.get('/index.html', function(request, response) {
+  response.sendFile(__dirname +'/index.html');
+});
+
+debugger
+
+app.post('/process_post', urlencodedParser,function(req, res){
+  response = {
+    first_name : req.body.first_name,
+    second_name : req.body.last_name
+  }
+
+  res.end(JSON.stringify(response));
+});
+app.get('/list_user', function(request, response){
+  console.log("Got a POST request for the homepage");
+  response.send("Page Listing");
+});
+
+app.post('/', function(request,response) {
+  console.log("Got a POST request for the homepage");
+  response.send('Hello Post');
+});
+
+var server = app.listen(8081, function(request,response) {
+  var host = server.address().address
+  var port = server.address().host
+
+  console.log("Example app listening at http://%s:%s", host, port)
+});
--- router.js
+++
@@ -1,8 +0,0 @@
-function route(handle,pathname,response,request) {
-  console.log("About to route a request for " + pathname);
-  if(typeof handle[pathname] == 'function') {
-    return handle[pathname](response,request);
-  }
-}
-
-exports.route = route;
